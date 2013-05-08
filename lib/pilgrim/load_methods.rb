2b1f229284cb18e97544acde72d3e797381d34f1
Pablo Marti <pablo@greenmediaagency.com>
2013-05-08 13:47:56 -0600

    Add option for pre-select country, state and city in dropdowns

--- lib/pilgrim/load_methods.rb
+++ lib/pilgrim/load_methods.rb
@@ -6,7 +6,8 @@ module Pilgrim
 
 	  module InstanceMethods
 	  	def get_countries
-	  		Pilgrim::Country.get_countries
+	  		countries = Pilgrim::Country.get_countries
+	  		return countries
 	  	end
 
 	  	def get_states(country_id)
@@ -19,11 +20,24 @@ module Pilgrim
 	  		state.cities
 	  	end
 	  	
-	    def get_locations
-  			@countries = get_countries
-  			@states = get_states(@countries.first.id)
-  			@cities = get_cities(@states.first.id)
+	    def get_locations(country_id = 0, state_id = 0, city_id = 0)
+  			countries = get_countries
+
+  			country_id = countries.first.id if country_id == 0
+  			@countries = transform_result(countries, country_id)
+  			states = get_states(country_id)
+
+  			state_id = states.first.id if state_id == 0 and !states.empty?
+  			@states = transform_result(states, state_id)
+  			cities = get_cities(state_id)
+
+  			city_id = cities.first.id if city_id == 0 and !cities.empty?
+  			@cities = transform_result(cities, city_id)
 	    end	    
+
+	    def transform_result(data, selected)
+	    	{data: data, selected: selected}
+	    end
 	  end
 
   end
