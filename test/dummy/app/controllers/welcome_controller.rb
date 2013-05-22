5dcd23bb9c9c7c929378b9874f86902fc18bafb5
Pablo Marti <pablo@greenmediaagency.com>
2013-05-22 14:47:24 -0600

    Custom countries per dropdown

--- test/dummy/app/controllers/welcome_controller.rb
+++ test/dummy/app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   
   def index
-  	get_locations
+  	@data1 = get_locations
+  	@data2 = get_locations({mode: "exclude", countries: ["Japan", "El Salvador", "Colombia", "Jamaica", "Spain"]}) #No hard feelings, it was the first name it came to my mind (I love Japan)
+  	@data3 = get_locations({mode: "include", countries: ["Japan", "El Salvador", "Colombia", "Jamaica", "Spain"]})
   end
 
 end
