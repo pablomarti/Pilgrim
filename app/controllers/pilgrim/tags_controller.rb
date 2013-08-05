051d5163f233bdd5fedc7ba69f94e7ab8659dd75
Pablo Marti <startpablo@debianhost.lan>
2013-08-04 20:48:10 -0600

    Concern for Tags controller

--- app/controllers/pilgrim/tags_controller.rb
+++ app/controllers/pilgrim/tags_controller.rb
@@ -3,35 +3,7 @@ require_dependency "pilgrim/application_controller"
 module Pilgrim
   class TagsController < ApplicationController
 
-	  def change_location
-	    @block = params[:block]
-	    @kind = params[:kind]
-	    @form_name = params[:form_name]
-        @city_name = params[:city_name]
-        @city_html_class = params[:city_html_class]
-	    value = params[:value]
-
-	    @states = nil
-	    @cities = nil
-
-	    if @kind == "country"
-	      @state_name = params[:state_name]
-	      @state_html_class = params[:state_html_class]
-	      
-	      country = Pilgrim::Country.find(value)
-	      
-	      states = country.states.order("name ASC") rescue []
-	      @states = transform_result(states, 0)
-	      
-	      cities = states.first.cities.order("name ASC") rescue []
-	      @cities = transform_result(cities, 0)
-	    elsif @kind == "state"
-	      state = Pilgrim::State.find(value)
-	      
-	      cities = state.cities.order("name ASC") rescue []
-	      @cities = transform_result(cities, 0)
-	    end
-	  end
+	  include Pilgrim::Concerns::Controllers::TagsController
 
   end
 end
--- lib/pilgrim.rb
+++ lib/pilgrim.rb
@@ -1,5 +1,7 @@
 require "pilgrim/engine"
 require "pilgrim/load_methods"
 
+Dir[File.dirname(__FILE__) + "/pilgrim/concerns/controllers/*.rb"].each{ |f| require "pilgrim/concerns/controllers/#{File.basename(f, ".rb")}" }
+
 module Pilgrim
 end
